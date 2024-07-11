6b327889828864a79794598174dc9c4006a65282
JunYeong-J <eeot2827@gmail.com>
2024-07-11 17:28:53 +0900

    메인화면에 scrollView 적용으로 화면 수정

--- WeatherApp/MainView/View/MainWeatherView.swift
+++ WeatherApp/MainView/View/MainWeatherView.swift
@@ -9,39 +9,85 @@ import UIKit
 import SnapKit
 
 final class MainWeatherView: BaseView {
+
+    let scrollView = UIScrollView()
+    let contentView = UIView()
     
     let cityNameLabel = UILabel()
     let temperatureLabel = UILabel()
     let weatherInformationLabel = UILabel()
     let highAndLowLabel = UILabel()
     
+    let threeHourView = UIView()
+    let fiveDayView = UIView()
+    let locationView = UIView()
+    let etcView = UIView()
     
     override func configureHierarchy() {
-        addSubview(cityNameLabel)
-        addSubview(temperatureLabel)
-        addSubview(weatherInformationLabel)
-        addSubview(highAndLowLabel)
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(cityNameLabel)
+        contentView.addSubview(temperatureLabel)
+        contentView.addSubview(weatherInformationLabel)
+        contentView.addSubview(highAndLowLabel)
+        contentView.addSubview(threeHourView)
+        contentView.addSubview(fiveDayView)
+        contentView.addSubview(locationView)
+        contentView.addSubview(etcView)
     }
     
     override func configureLayout() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.width.equalTo(scrollView.snp.width)
+            make.verticalEdges.equalTo(scrollView)
+        }
+        
         cityNameLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(safeAreaLayoutGuide)
-            make.top.equalTo(safeAreaLayoutGuide).inset(20)
+            make.centerX.equalTo(contentView)
+            make.top.equalTo(contentView).inset(20)
         }
         
         temperatureLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(safeAreaLayoutGuide)
+            make.centerX.equalTo(contentView)
             make.top.equalTo(cityNameLabel.snp.bottom).offset(1)
         }
         
         weatherInformationLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(safeAreaLayoutGuide)
+            make.centerX.equalTo(contentView)
             make.top.equalTo(temperatureLabel.snp.bottom).offset(1)
         }
         
         highAndLowLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(safeAreaLayoutGuide)
+            make.centerX.equalTo(contentView)
             make.top.equalTo(weatherInformationLabel.snp.bottom).offset(1)
+        }
+        
+        threeHourView.snp.makeConstraints { make in
+            make.top.equalTo(highAndLowLabel.snp.bottom).offset(50)
+            make.horizontalEdges.equalTo(contentView).inset(20)
+            make.height.equalTo(300)
+        }
+        
+        fiveDayView.snp.makeConstraints { make in
+            make.top.equalTo(threeHourView.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(contentView).inset(20)
+            make.height.equalTo(500)
+        }
+        
+        locationView.snp.makeConstraints { make in
+            make.top.equalTo(fiveDayView.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(contentView).inset(20)
+            make.height.equalTo(300)
+        }
+        
+        etcView.snp.makeConstraints { make in
+            make.top.equalTo(locationView.snp.bottom).offset(10)
+            make.bottom.horizontalEdges.equalTo(contentView).inset(20)
+            make.height.equalTo(500)
         }
     }
     
@@ -50,6 +96,13 @@ final class MainWeatherView: BaseView {
         temperatureLabel.setUILabel("", textAlignment: .center, color: .black, backgroundColor: .clear, font: .boldSystemFont(ofSize: 70), cornerRadius: 0, numberLine: 0)
         weatherInformationLabel.setUILabel("", textAlignment: .center, color: .black, backgroundColor: .clear, font: .boldSystemFont(ofSize: 30), cornerRadius: 0, numberLine: 0)
         highAndLowLabel.setUILabel("", textAlignment: .center, color: .black, backgroundColor: .clear, font: .boldSystemFont(ofSize: 20), cornerRadius: 0, numberLine: 0)
+        
+        threeHourView.backgroundColor = .blue
+        fiveDayView.backgroundColor = .green
+        locationView.backgroundColor = .red
+        etcView.backgroundColor = .brown
+        
+        
     }
     
 }
