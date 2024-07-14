33a3660abb9239117a4d1d6df4024055afff379b
JunYeong-J <eeot2827@gmail.com>
2024-07-14 23:59:36 +0900

    API요청 코드 수정

--- WeatherApp/Network/WeatherAPIManager.swift
+++ WeatherApp/Network/WeatherAPIManager.swift
@@ -15,6 +15,7 @@ final class WeatherAPIManager {
     private init() { }
     
     typealias OpenWeatherHandler = (OpenWeather?, OpenWeatherError?) -> Void
+    typealias OpenWeatherThreeHourHandler = (ThreeHourWeather?, OpenWeatherError?) -> Void
     
     func mainWeatherRequest<T: Decodable>(api: WeatherRequest, model: T.Type, completionHandler: @escaping OpenWeatherHandler) {
         AF.request(api.endPoint, method: api.method,
@@ -33,30 +34,19 @@ final class WeatherAPIManager {
         }
     }
     
-    
-    
-    func fetchWeatherAPI(completionHandler: @escaping (OpenWeather) -> Void) {
-        let url = "\(APIURL.openWeatherURL)\(APIKey.openWeatherID)"
-        
-        AF.request(url).responseDecodable(of: OpenWeather.self) { response in
-            switch response.result {
-            case .success(let success):
-                completionHandler(success)
-            case .failure(let failure):
-                print(failure)
-            }
-        }
-    }
-    
-    func fetchThreeHourWeatherAPI(completionHandler: @escaping (ThreeHourWeather) -> Void) {
-        let url = "\(APIURL.threeHourWeatherURL)\(APIKey.openWeatherID)"
-        
-        AF.request(url).responseDecodable(of: ThreeHourWeather.self) { response in
-            switch response.result {
-            case .success(let success):
-                completionHandler(success)
-            case .failure(let failure):
-                print(failure)
+    func fetchThreeHourWeatherAPI<T: Decodable>(api: WeatherRequest, model: T.Type, completionHandler: @escaping OpenWeatherThreeHourHandler) {
+        AF.request(api.endPoint, method: api.method,
+                   parameters: api.parameter,
+                   encoding: URLEncoding(destination: .queryString)
+        )
+        .validate(statusCode: 200..<500)
+        .responseDecodable(of: ThreeHourWeather.self) { response in
+            switch response.result{
+            case .success(let value):
+                completionHandler(value, nil)
+            case .failure(let error):
+                completionHandler(nil, .failedRequest)
+                print(error)
             }
         }
     }
