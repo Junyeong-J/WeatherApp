e0c6c66466e16b646b62ac35e4b6b8f9d8820f7d
JunYeong-J <eeot2827@gmail.com>
2024-07-15 01:42:11 +0900

    기타 정보화면 설정

--- WeatherApp/MainView/View/EtcCollectionViewCell.swift
+++ WeatherApp/MainView/View/EtcCollectionViewCell.swift
@@ -5,4 +5,43 @@
 //  Created by 전준영 on 7/14/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class EtcCollectionViewCell: BaseCollectionViewCell {
+    
+    let titleLabel = UILabel()
+    let infoLabel = UILabel()
+    let moreInfoLabel = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(infoLabel)
+        contentView.addSubview(moreInfoLabel)
+    }
+    
+    override func configureLayout() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.leading.equalTo(contentView).inset(15)
+        }
+        
+        infoLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.leading.equalTo(contentView).inset(15)
+        }
+        
+        moreInfoLabel.snp.makeConstraints { make in
+            make.leading.bottom.equalTo(contentView).inset(15)
+        }
+    }
+    
+    override func configureView() {
+        titleLabel.setUILabel("타이틀", textAlignment: .left, color: .darkGray, backgroundColor: .clear, font: .systemFont(ofSize: 20), cornerRadius: 0, numberLine: 0)
+        infoLabel.setUILabel("30%", textAlignment: .left, color: .black, backgroundColor: .clear, font: .boldSystemFont(ofSize: 28), cornerRadius: 0, numberLine: 0)
+        moreInfoLabel.setUILabel("강풍: 000", textAlignment: .left, color: .black, backgroundColor: .clear, font: .systemFont(ofSize: 22), cornerRadius: 0, numberLine: 0)
+    }
+    
+    func configureData(data: ETCType) {
+        titleLabel.text = data.rawValue
+    }
+}
